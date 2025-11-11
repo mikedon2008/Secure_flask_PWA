21406723f142578266116fd9a754f6dd9103c14c
Mike DON <nguyen.don225@education.nsw.gov.au>
2025-11-12 10:04:08 +1100

    Add css to the file

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.secret_key = os.urandom(24)
 # Define the route for the home page
 @app.route('/')
 def login():
-    return "hello world"
+    return render_template(login.html)
 
 # Run the app in debug mode
 if __name__ == '__main__':
