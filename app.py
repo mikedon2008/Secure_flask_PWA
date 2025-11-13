ebd4b36fe6cf2247ccb51205bd3f4b14640981e2
Mike DON <nguyen.don225@education.nsw.gov.au>
2025-11-13 14:49:25 +1100

    Adding users to the database

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import os
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-# Define the route for the login page
+# Route for login page
 @app.route('/')
 def login():
     return render_template("login.html")
 
-# Define the route for login validation
+# Route for validating login credentials
 @app.route('/login_validation', methods=['POST'])
 def login_validation():
     email = request.form.get('email')
@@ -24,12 +24,11 @@ def login_validation():
     connection.close()
 
     if len(user) > 0:
-        # Redirect to /home with user details as URL parameters
         return redirect(f'/home?fname={user[0][0]}&lname={user[0][1]}&email={user[0][2]}')
     else:
         return redirect('/')
 
-# Define the route for the home page after login
+# Route for home page after login
 @app.route('/home')
 def home():
     fname = request.args.get('fname')
@@ -38,6 +37,29 @@ def home():
 
     return render_template('home.html', fname=fname, lname=lname, email=email)
 
-# Run the app in debug mode
+# Route for handling sign-up form and adding user to database
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    connection = sqlite3.connect('LoginData.db')
+    cursor = connection.cursor()
+
+    # Check if user already exists
+    ans = cursor.execute("SELECT * FROM USERS WHERE email=? AND password=?", (email, password)).fetchall()
+
+    if len(ans) > 0:
+        connection.close()
+        return render_template('login.html')
+    else:
+        cursor.execute("INSERT INTO USERS(fname, lname, email, password) VALUES (?, ?, ?, ?)", (fname, lname, email, password))
+        connection.commit()
+        connection.close()
+        return render_template('login.html')
+
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
